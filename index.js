47137649f29fd626b69b7d04624f545c013aa7a2
Ian King <ian.antking@gmail.com>
2019-06-09 14:52:44 +0100

    updated file name

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Flag = require('./src/flags');
-const Color = require('./src/colors');
+const Flag = require('./src/flag');
+const Color = require('./src/color');
 
 exports.Color = Color;
 exports.Flag = Flag;
