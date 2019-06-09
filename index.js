ba2e33a00cf91ceff15c6aa4ae3567b13daacba6
Ian King <ian.antking@gmail.com>
2019-06-09 11:23:36 +0100

    changed export

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 const Flags = require('./src/flags');
 const Colors = require('./src/colors');
 
-(function exportPride() {
-  const Pride = {
-    colors: Colors,
-    flags: Flags,
-  };
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = Pride;
-  } else {
-    window.Pride = Pride;
-  }
-}());
+exports.Colors = Colors;
+exports.Flags = Flags;
