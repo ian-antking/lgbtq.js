c6885c72075bc7a0b3b2c96a511377d1bec099f5
ian-antking <ian.antking@gmail.com>
2020-07-17 20:58:10 +0100

    add second test case

--- __tests__/gradient-builder.assets.js
+++ __tests__/gradient-builder.assets.js
@@ -1,5 +1,5 @@
-import  { buildGradient, flags }  from '../index';
-import transFlagGradient from './trans-flag-gradient';
+import  { buildGradient }  from '../index';
+import { transFlagGradient, prideFlagGradient } from './expected';
 
 describe('buildGradient', () => {
   it('returns a css gradient', () => {
@@ -9,9 +9,25 @@ describe('buildGradient', () => {
       '#ffffff',
       '#f7a8b8',
       '#55cdfc',
-    ]
+    ];
     const expected = transFlagGradient;
 
     expect(buildGradient(colors)).toBe(expected)
+  });
+
+  it('can handle different numbers of stripes', () => {
+    const colors = [
+      '#000000',
+      '#784F0C',
+      '#E70000',
+      '#FF8C00',
+      '#FFEF00',
+      '#00811F',
+      '#0044FF',
+      '#760089',
+    ];
+    const expected = prideFlagGradient;
+
+    expect(buildGradient(colors)).toBe(prideFlagGradient);
   });
 });
