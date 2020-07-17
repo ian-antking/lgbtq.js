9fc2fd60e54f6dee2ba1657c6052b8d90b25e967
ian-antking <ian.antking@gmail.com>
2020-07-17 21:26:32 +0100

    buildGradient now takes the name of a flag

--- __tests__/gradient-builder.assets.js
+++ __tests__/gradient-builder.assets.js
@@ -1,33 +1,9 @@
 import  { buildGradient }  from '../index';
-import { transFlagGradient, prideFlagGradient } from './expected';
+import { transFlagGradient, inclusivePrideFlagGradient } from './expected';
 
 describe('buildGradient', () => {
-  it('returns a css gradient', () => {
-    const colors = [
-      '#55cdfc',
-      '#f7a8b8',
-      '#ffffff',
-      '#f7a8b8',
-      '#55cdfc',
-    ];
-    const expected = transFlagGradient;
-
-    expect(buildGradient(colors)).toBe(expected)
-  });
-
-  it('can handle different numbers of stripes', () => {
-    const colors = [
-      '#000000',
-      '#784F0C',
-      '#E70000',
-      '#FF8C00',
-      '#FFEF00',
-      '#00811F',
-      '#0044FF',
-      '#760089',
-    ];
-    const expected = prideFlagGradient;
-
-    expect(buildGradient(colors)).toBe(prideFlagGradient);
+  it('takes a flag name and returns a css gradient', () => {
+    expect(buildGradient('trans')).toBe(transFlagGradient);
+    expect(buildGradient('inclusivePride')).toBe(inclusivePrideFlagGradient);
   });
 });
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import colors from "./lib/colors";
-import flags from "./lib/flags";
 import buildGradient from './lib/build-gradient';
 
 export {
   colors,
-  flags,
   buildGradient
 }
