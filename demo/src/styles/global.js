1718f475f181ddb7d280d9366a17b31b08566fe6
ian-antking <ian.antking@gmail.com>
2020-07-20 08:51:14 +0100

    clean up src and remove unused files

---
+++ demo/src/styles/global.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+  body {
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
