8cdd9c0fe60c6652c3238ee55283f23ea1e2670b
ian-antking <ian.antking@gmail.com>
2020-07-21 12:02:00 +0100

    run linting

--- __tests__/expected.js
+++ __tests__/expected.js
@@ -1,7 +1,7 @@
-const transFlagGradient = `linear-gradient(180deg,#55cdfc 0%,#55cdfc 20%,#f7a8b8 20%,#f7a8b8 40%,#ffffff 40%,#ffffff 60%,#f7a8b8 60%,#f7a8b8 80%,#55cdfc 80%,#55cdfc 100%)`;
+const transFlagGradient = 'linear-gradient(180deg,#55cdfc 0%,#55cdfc 20%,#f7a8b8 20%,#f7a8b8 40%,#ffffff 40%,#ffffff 60%,#f7a8b8 60%,#f7a8b8 80%,#55cdfc 80%,#55cdfc 100%)';
 const inclusivePrideFlagGradient = 'linear-gradient(180deg,#000000 0%,#000000 12.5%,#784F0C 12.5%,#784F0C 25%,#E70000 25%,#E70000 37.5%,#FF8C00 37.5%,#FF8C00 50%,#FFEF00 50%,#FFEF00 62.5%,#00811F 62.5%,#00811F 75%,#0044FF 75%,#0044FF 87.5%,#760089 87.5%,#760089 100%)';
 
 export {
   transFlagGradient,
   inclusivePrideFlagGradient,
-}
+};
