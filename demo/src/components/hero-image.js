a93031b91f604f3c324aa439e4afa19ba51ae938
ian-antking <ian.antking@gmail.com>
2020-07-21 14:20:25 +0100

    use react-lazy-hero

--- demo/src/components/hero-image.js
+++ demo/src/components/hero-image.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
+import LazyHero from 'react-lazy-hero';
 import randomFlag from '../helpers/random-flag';
-
-const Container = styled.div`
-  height: 50vh;
-  background: ${props => props.background};
-`
+import Logo from './logo';
 
 const HeroImage = () => {
   const [ flag, setFlag ] = useState(randomFlag());
 
   useEffect(() => {
-    const changeFlag = window.setInterval(() => setFlag(randomFlag()), 5000)
+    const changeFlag = window.setInterval(() => setFlag(randomFlag()), 10000)
     return () => window.clearInterval(changeFlag);
   })
 
   return (
-    <Container  background={flag}/>
+    <LazyHero
+      imageSrc=""
+      opacity={0.5}
+      style={{
+        background: flag,
+      }}
+    >
+      <Logo />
+    </LazyHero>
   )
 };
 
