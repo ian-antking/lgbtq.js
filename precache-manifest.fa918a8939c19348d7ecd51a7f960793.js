23b21e74822f51732f477e95c45ebee51fcd3759
ian-antking <ian.antking@gmail.com>
2020-08-17 23:16:07 +0100

    Updates

---
+++ precache-manifest.fa918a8939c19348d7ecd51a7f960793.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f08900fe01582d4160eeb7af79b99a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7847c59385c0b813c916",
+    "url": "/static/js/2.628ddb5d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.628ddb5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "550e9c81b654e89b71fd",
+    "url": "/static/js/main.c5477cc4.chunk.js"
+  },
+  {
+    "revision": "49de1d56170d62cf3751",
+    "url": "/static/js/runtime-main.d0798475.js"
+  }
+]);
