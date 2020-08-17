bb4d8309409059bf7f3005212a947fdbfab55b30
ian-antking <ian.antking@gmail.com>
2020-08-17 23:19:30 +0100

    Updates

---
+++ precache-manifest.85b305878ba160f0b01d6b7bc82d59c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a29845da6bcc3bcdbba8b3198123581",
+    "url": "/lgbtq.js/index.html"
+  },
+  {
+    "revision": "aa7605aca630bb66a1cf",
+    "url": "/lgbtq.js/static/js/2.83b014df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lgbtq.js/static/js/2.83b014df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "211f1e036e5466f8278d",
+    "url": "/lgbtq.js/static/js/main.cc51c95d.chunk.js"
+  },
+  {
+    "revision": "b07dde05da30060d3ce8",
+    "url": "/lgbtq.js/static/js/runtime-main.66c89b01.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa918a8939c19348d7ecd51a7f960793.js"
+  "/lgbtq.js/precache-manifest.85b305878ba160f0b01d6b7bc82d59c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lgbtq.js/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
