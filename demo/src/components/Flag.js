db8dbf27c14b7fe1e3386a55f160148e84b35e7c
ian-antking <ian.antking@gmail.com>
2020-08-17 23:04:44 +0100

    change FlagTitle to Subtitle

--- demo/src/components/Flag.js
+++ demo/src/components/Flag.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import FlagTitle from './FlagTitle';
+import FlagTitle from './Subtitle';
 import { buildFlag } from 'lgbtq.js';
 
 const FlagContainer = styled.div`
