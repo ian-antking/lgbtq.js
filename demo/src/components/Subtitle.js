7937c0be1e501a576bfd9f154a0879133f602fca
ian-antking <ian.antking@gmail.com>
2020-08-18 22:16:51 +0100

    add Subtitle components

--- demo/src/components/Subtitle.js
+++ demo/src/components/Subtitle.js
@@ -1,38 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const TitleContainer = styled.div`
+const Subtitle = styled.span`
+  width: 100%;
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
 `;
-
-const TitleText = styled.h2`
-  font-family: 'Lobster', cursive;
-  font-size: 2em;
-  background-color: #808080;
-  padding: 8px;
-  border-radius: 10px;
-  border: 2px solid white;
-  color: ${props => props.color};
-`;
-
-const Subtitle = ({ title, color }) => (
-  <TitleContainer>
-    <TitleText color={color}>
-      {title}
-    </TitleText>
-  </TitleContainer>
-);
-
-Subtitle.defaultProps = {
-  color: '#000000'
-}
-
-Subtitle.propTypes = {
-  title: PropTypes.string.isRequired,
-  color: PropTypes.string,
-}
 
 export default Subtitle;
