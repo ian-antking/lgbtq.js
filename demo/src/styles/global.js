7d47b27993fa03cb92624c894d48943d5eb3c040
ian-antking <ian.antking@gmail.com>
2020-08-18 22:17:47 +0100

    text defaults to white

--- demo/src/styles/global.js
+++ demo/src/styles/global.js
@@ -7,5 +7,6 @@ export default createGlobalStyle`
     font-family: 'Roboto', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    color: white;
   }
 `;
