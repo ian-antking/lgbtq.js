28bcacc2a5920925277473796f4aec5dcff2e46b
ian-antking <ian.antking@gmail.com>
2021-03-13 13:44:26 +0000

    add jsdoc to functions

--- module/lib/build-flag.js
+++ module/lib/build-flag.js
@@ -1,7 +1,15 @@
 import flags from './flags';
 import convertOpacity from './convert-opacity';
 
-export default ({ flag, background, opacity = '' }) => { // eslint-disable-next-line dot-notation
+/**
+ * @public
+ * @param {object} [options] - Configuration for the flag to render.
+ * @param {string} [options.flag] - The name of the flag to render. 
+ * @param {string} [options.background] - URL of image to render underneath flag.
+ * @param {number} [options.opacity] - Opacity of flag as float between 0 and 1.
+ * @returns {string} - CSS gradient string.
+ */
+const buildFlag = ({ flag, background, opacity = '' }) => { // eslint-disable-next-line dot-notation
   const colors = flags[flag] || flags.inclusivePride;
   const hexOpacity = opacity ? convertOpacity(opacity) : null;
   const backgroundUrl = background ? `, url(${background})` : '';
@@ -13,3 +21,5 @@ export default ({ flag, background, opacity = '' }) => { // eslint-disable-next-
 
   return `linear-gradient(180deg,${colorSteps})${backgroundUrl}`;
 };
+
+export default buildFlag;
--- module/lib/convert-opacity.js
+++ module/lib/convert-opacity.js
@@ -1,5 +1,12 @@
-export default (decimal) => {
+/**
+ * @private
+ * @param {number} decimal - The decimal to convert to hex. 
+ * @returns {string} - hex value.
+ */
+const convertOpacity = (decimal) => {
   const alpha = Math.round(decimal * 255);
   const hex = (alpha + 0x10000).toString(16).substr(-2);
   return hex;
 };
+
+export default convertOpacity;
